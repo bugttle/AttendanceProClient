39d63c34e4f0855f277e7ab91d3760a5394fa96d
UQ Times <uqtimes@gmail.com>
2017-03-01 10:34:00 +0900

    二重起動を防止するための処理追加

--- AttendanceProClient/Program.cs
+++ AttendanceProClient/Program.cs
@@ -1,22 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
-namespace AttendanceProClient {
-    static class Program {
+namespace AttendanceProClient
+{
+    static class Program
+    {
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
         [STAThread]
-        static void Main() {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+        static void Main()
+        {
+            // Mutexを利用して、二重起動を防止
+            bool createdNew;
+            var mutex = new Mutex(true, Application.ProductName, out createdNew);
+            if (!createdNew)
+            {
+                MessageBox.Show("既に起動しています。");
+                mutex.Close();
+                return;
+            }
 
-            // 起動時にFormを表示したくないので、Runメソッドに渡さない
-            new Form1();
-            Application.Run();
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                // 起動時にFormを表示したくないので、Runメソッドに渡さない
+                new Form1();
+                Application.Run();
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+                mutex.Close();
+            }
         }
     }
 }
